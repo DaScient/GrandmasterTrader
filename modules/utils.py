f7d38d81cda6be79319af39862f265ced5d5cc9e
dascient <25983786+DaScient@users.noreply.github.com>
2025-07-15 09:40:06 -0600

    Create utils.py

---
+++ modules/utils.py
@@ -0,0 +1,13 @@
+# File: modules/utils.py
+
+ASSETS = [
+    # Liquid US stocks & OTC picks
+    "AAPL", "MSFT", "GOOGL", "TSLA", "NVDA",
+    "WDLF", "RKLB", "RYCEY",
+
+    # Crypto
+    "BTC-USD", "ETH-USD", "BNB-USD", "ADA-USD",
+
+    # Forex
+    "EURUSD=X", "GBPUSD=X", "USDJPY=X"
+]
